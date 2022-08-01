e0ec4cef6c2555688f718afed9d87c573a7f82be
Enricopro <enridalberton@gmail.com>
2022-08-01 18:58:04 +0200

    Draft of the header done

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
+import { FaDiscord } from 'react-icons/fa';
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="w-screen flex flex-row items-center justify-between h-16 px-5">
+      <p className="text-secondary">LOGO</p>
+      <a href="https://discord.gg/Kv7hyVFs" className="w-29 h-10 bg-sky-600 hover:bg-sky-700 flex flex-row items-center px-2 pr-4 rounded-md transition-all">
+        <FaDiscord className="text-secondary w-10"/>
+        <p className="text-secondary text-s">Join our Discord</p>
+      </a>  
+    </div>
+    
   )
 }
