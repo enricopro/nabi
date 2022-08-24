2bea48ffde73df9ff44682fdb7f5de091d22ada0
Enricopro <enridalberton@gmail.com>
2022-08-24 09:25:39 +0200

    Added some elements to Home and started Discover

--- src/Discover.js
+++ src/Discover.js
@@ -1,5 +1,35 @@
+import { FaDiscord, FaTwitter } from 'react-icons/fa';
+import handcuff from './img/handcuff.png';
+
 export default function Discover() {
   return (
-    <h1>Discover</h1>
+    <>
+      <div className="w-screen flex flex-row items-center justify-between h-16 px-5 absolute top-0 fadeIn">
+        <div className="flex flex-row text-secondary w-full items-center">
+          <p>product by</p>
+          <img className="ml-2 w-14" src={require("./img/logo.png")} alt="logo"/>
+        </div>
+        <div className="flex flex-row items-center">
+          <a href="https://discord.gg/Kv7hyVFs" className="z-10">
+            <FaDiscord size={22} className="text-secondary mx-2 hover:text-gray-300 transition-all"/>
+          </a>
+          <a href="https://discord.gg/Kv7hyVFs" className="z-10">
+            <FaTwitter size={22} className="text-secondary mx-2 hover:text-gray-300 transition-all"/>
+          </a>
+        </div>
+      </div>
+      <div className="w-11/12 max-w-[1390px] flex flex-col mx-auto mt-10 fadeIn">
+        <div className="w-full flex flex-row bg-black tracking-tighter min-h-[490px] justify-between items-center">
+          <div className="flex flex-col items-center justify-center w-1/2 bg-primary rounded-3xl p-10 h-full mr-5">
+            <h2 className="text-secondary text-3xl font-semibold">What is a <span className="text-slate-300">controlnode</span>?</h2>
+            <br/>
+            <p className="text-secondary text-2xl font-semibold px-8">A controlnode is a single point of control over some parts of your digital life.<br/>
+            The three pillars of your digital life are your data, your identity and your connections.<br/><br/>You can be either aware or unaware, but everyone has at least one controlnode.<br/><br/>You can own it, or you can rent it - which really means that someone else owns it.</p>
+          </div>
+          <div className="w-1/2 rounded-3xl ml-5 fadeIn" style={{backgroundImage: `url(${handcuff})`}}>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import { FaDiscord, FaTwitter } from 'react-icons/fa';
 import { BiChevronsDown } from 'react-icons/bi';
+import { FiExternalLink } from 'react-icons/fi'
 import React, { useState, useEffect } from 'react';
 import firstp from './img/firstp.png';
-import macro from './img/macro.jpg';
-import bringithome from './img/bringithome.png';
-import { ReactComponent as Logo } from './img/logo.svg';
+import macro from './img/macro.gif';
+import bringithome from './img/bringithome.jpg';
+import { Link } from "react-router-dom";
 
 export default function Home() {
 
@@ -24,7 +25,10 @@ export default function Home() {
     if(clicked) {
       return <>
         <div className="w-screen flex flex-row items-center justify-between h-16 px-5 absolute top-0 fadeIn">
-          <Logo fill='red' stroke='green' className="bg-secondary h-6"/>
+          <div className="flex flex-row text-secondary w-full items-center">
+            <p>product by</p>
+            <img className="ml-2 w-14" src={require("./img/logo.png")} alt="logo"/>
+          </div>
           <div className="flex flex-row items-center">
             <a href="https://discord.gg/Kv7hyVFs" className="z-10">
               <FaDiscord size={22} className="text-secondary mx-2 hover:text-gray-300 transition-all"/>
@@ -35,13 +39,13 @@ export default function Home() {
           </div>
         </div>
         <div className="h-screen w-screen flex flex-col justify-between items-center absolute top-0 fadeIn tracking-tighter font-semibold">
-          <h1 className="text-transparent text-7xl mt-10 font-normal bg-clip-text bg-gradient-to-t to-gray-500 from-indigo-200">Cubit</h1>
-          <p className="text-secondary text-2xl mb-[2rem]">this is your personal controlnode</p>
+          <h1 className="text-transparent text-7xl mt-10 font-normal bg-clip-text bg-gradient-to-t to-gray-500 from-indigo-200 tracking-tight">Cubit</h1>
+          <p className="text-secondary text-3xl mb-[2rem]">your personal controlnode</p>
         </div>
         <div className="w-11/12 max-w-[1390px] flex flex-col mx-auto mt-10 fadeIn">
           <div className="w-full flex flex-row bg-black tracking-tighter h-[490px] justify-between items-center">
             <div className="flex flex-col items-center justify-center w-1/2 bg-primary rounded-3xl p-10 h-full mr-5">
-              <h2 className="text-secondary text-3xl font-semibold">What is a <span className="text-slate-300">controlnode</span>?</h2>
+              <h2 className="text-secondary text-3xl font-semibold">What is a controlnode?</h2>
               <br/>
               <p className="text-secondary text-2xl font-semibold px-8">A controlnode is a single point of control over some parts of your digital life.<br/>
               The three pillars of your digital life are your data, your identity and your connections.<br/><br/>You can be either aware or unaware, but everyone has at least one controlnode.<br/><br/>You can own it, or you can rent it - which really means that someone else owns it.</p>
@@ -60,21 +64,27 @@ export default function Home() {
             </div>
           </div>
           <div className="w-full flex flex-col justify-center items-center mx-auto my-10 bg-primary min-h-[490px] bg-cover rounded-3xl fadeIn" style={{backgroundImage: `url(${macro})`}}>
-            <h2 className="text-5xl font-semibold text-secondary pt-7">How cubit will serve you</h2>
+            <h2 className="text-[4rem] font-semibold text-secondary pt-7 mb-5">How cubit will serve you</h2>
             <div className="w-full flex flex-col justify-end items-end mx-auto">
               <br/>
               <div className="flex flex-col items-start justify-center w-1/2 rounded-3xl p-10 h-full mr-5 text-secondary text-left">
                 <div>
-                  <p className="text-2xl font-semibold hover:cursor-pointer my-3 peer transition-all">Network Attached Storage</p>
+                  <p className="text-2xl font-semibold my-3 peer transition-all">Network Attached Storage</p>
                   <p className="opacity-0 peer-hover:opacity-100 transition-all">You data in one secure place, easily accessible at any time</p>
                 </div>
                 <div>
-                  <p className="text-2xl font-semibold hover:cursor-pointer my-3 peer transition-all">ION node</p>
+                  <a href="https://identity.foundation/ion/" target="_blank" rel="noreferrer" className="flex flex-row items-center peer hover:cursor-pointer">
+                    <p className="text-2xl font-semibold hover:cursor-pointer my-3 peer transition-all">ION Node</p>
+                    <FiExternalLink size={22} className="text-secondary ml-2"/>
+                  </a>
                   <p className="opacity-0 peer-hover:opacity-100 transition-all">Identity is an important topic. Do not trust, verify by yourself!</p>
                 </div>
                 <div>
-                  <p className="text-2xl font-semibold hover:cursor-pointer my-3 peer transition-all">Bitcoin full node</p>
-                  <p className="opacity-0 peer-hover:opacity-100 transition-all">Help secure the most decentralised blockchain, while eliminating all third parties</p>
+                  <a href="https://bitcoin.org/en/full-node" target="_blank" rel="noreferrer" className="flex flex-row items-center peer hover:cursor-pointer">
+                    <p className="text-2xl font-semibold hover:cursor-pointer my-3 peer transition-all">Bitcoin Full Node</p>
+                    <FiExternalLink size={22} className="text-secondary ml-2"/>
+                  </a>
+                  <p className="opacity-0 peer-hover:opacity-100 transition-all">Help secure the most decentralised blockchain, while eliminating all third parties.</p>
                 </div>
                 <div>
                   <p className="text-2xl font-semibold hover:cursor-pointer my-3 peer transition-all">Password manager</p>
@@ -93,13 +103,22 @@ export default function Home() {
             </div>
           </div>
           <div className="w-full flex flex-row justify-center items-center mx-auto mb-10 bg-primary min-h-[100px] rounded-3xl fadeIn">
-            <div className="w-1/5 text-secondary flex items-center justify-center">
-              LOGO
+            <div className="w-1/5 p-10 text-secondary flex items-center justify-center">
+              <img className="" src={require("./img/logo.png")} alt="logo"/>
             </div>
             <div className="w-4/5">
-              <p className="text-secondary text-2xl font-semibold p-10">Your Google account has a controlnode. Your data, your messages, your interests are all tied, in one way or the other, to this account.<br/>This account is a contract between you and Google.<br/>You can be banned, censored, excluded and used, all withouth crossing the agreement's limits.<br/><br/>cubit is a controlnode that you own.</p>
+              <p className="text-secondary text-2xl font-semibold p-10">nabi is an early project with a simple Mission. Build a new Internet where the induvidual is at the core, not at the perifery. Where technology is a tool for people, not the end goal. Where we can all experience freedom, true freedom.</p>
             </div>
           </div>
+          <div className="w-full flex flex-row justify-between items-center mx-auto mb-10 min-h-[50px] rounded-3xl fadeIn">
+            <Link to="discover" className="text-secondary bg-primary font-bold p-5 rounded-3xl hover:bg-gray-900 transition-all">Discover our vision</Link>
+            <a href="https://discord.gg/Kv7hyVFs" className="p-5 bg-[#5865F2] rounded-3xl hover:bg-blue-500 text-white font-bold transition-all">Lear more on Discord</a>
+          </div>
+        </div>
+        <div className="w-full flex flex-col py-8 first-letter:justify-center bg-primary items-center mx-auto min-h-[100px] fadeIn">
+          <p className="text-secondary">Renders made by Permeteo.</p>
+          <p className="text-secondary">Logo made by Anna.</p>
+          <p className="text-secondary">With ❤️ from 🇮🇹</p>
         </div>
       </>
     } else {
@@ -109,8 +128,8 @@ export default function Home() {
 
   return (
     <>
-      <div className="w-screen h-screen bg-black flex justify-center realtive top-0 group fadeIn">
-        <img className={`md:h-screen w-screen object-cover ${clicked ? 'blur-none transition-all 2s' : 'blur-sm'}`} src={require("./img/cubit.jpg")} alt="cubit"/>
+      <div className="w-screen justify-center bg-black flex realtive top-0 group fadeIn">
+        <img className={` w-screen object-cover ${clicked ? 'blur-none transition-all 2s' : 'blur-sm'}`} src={require("./img/cubit2.jpg")} alt="cubit"/>
         <BiChevronsDown onClick={() => setClicked(true)} size={30} className={`text-secondary absolute bottom-10 hover:scale-110 transition-all hover:cursor-pointer z-20 ${clicked ? 'opacity-0' : 'opacity-100'}`} />
       </div>
       
