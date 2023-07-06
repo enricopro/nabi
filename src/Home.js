746d2a962095b40ef96408dc6de4a4efcc827d6c
Enricopro <enridalberton@gmail.com>
2023-07-06 10:31:34 +0200

    graphic bug fixed

--- src/Home.js
+++ src/Home.js
@@ -41,11 +41,11 @@ export default function Home() {
       <img src={newLogo} className="absolute h-20 z-10 top-7 left-7" alt="nabi_logo"/>
       <img src={background} className='absolute top-0 w-screen h-screen object-cover blur' alt="cubit"/>
       <h2 className="text-primary text-3xl px-2 ld:text-4xl text-center z-10 mb-5">Welcome to the <i>new internet</i><br/>—one that’s powered by <i>you</i>.</h2>
-      <div className="flex md:flex-col flex-row justify-center place-content-between w-screen bottom-20 fixed px-5">
+      <div className="flex md:flex-col flex-row justify-center place-content-between w-screen bottom-0 ld:bottom-20 fixed px-5">
         <a href="https://geyser.fund/project/nabitechnology" className="text-primary text-3xl text-center z-10 mb-10 mr-[30%] md:mr-[0px] underline">Geyser crowdfounding</a>
         <div className="flex flex-col justify-center items-center z-10 ">
           <div className="flex flex-row justify-center">
-            <input type="email" onChange={(e) => onChangeEmail(e)} className={`w-60 rounded-3xl pl-6 text-secondary ${errorInEmail ? 'bg-red-100' : 'bg-gray-100'}`} placeholder="Email address" />
+            <input type="email" onChange={(e) => onChangeEmail(e)} className={`w-56 rounded-3xl pl-6 text-secondary ${errorInEmail ? 'bg-red-100' : 'bg-gray-100'}`} placeholder="Email address" />
             <button onClick={() => subscribe()} className="rounded-3xl font-bold p-3 px-4 ml-2 text-white bg-[#5865F2] hover:bg-blue-500 transition-all">Keep me updated!</button>
           </div>
           <p className={`text-primary text-center mt-5 z-10 ${emailSubmitted ? 'opacity-100' : 'opacity-0'} transition-all`}>You have successfully submitted your email address!</p>
